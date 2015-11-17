bdf5ba2be26d87ccb6faedfdeb9c1c20d860266c
bys-galen-zhang <galen.zhang@beyondsoft.com>
2015-11-17 15:55:34 +0800

    Fixed spark error: javax.servlet.FilterRegistration signer information does notmatch

--- src/main/scala/com/achievo/spark/rdd/SimpleApp.scala
+++ src/main/scala/com/achievo/spark/rdd/SimpleApp.scala
@@ -7,8 +7,8 @@ import org.apache.spark.SparkContext
  */
 object SimpleApp {
   def main(args: Array[String]): Unit = {
-    val logFile = "file///home/hadoop/temp/README.md"
-    val conf = new SparkConf().setAppName("Simple Application")
+    val logFile = "file:///home/hadoop/temp/README.md"
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
